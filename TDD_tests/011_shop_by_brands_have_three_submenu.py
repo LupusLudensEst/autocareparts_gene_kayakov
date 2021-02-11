f979999657bda52d5f63e8d692608d4d61327c29
Viacheslav Gurov <gurov>
2021-02-10 23:14:27 -0500

    dt 10 feb 2021

---
+++ TDD_tests/011_shop_by_brands_have_three_submenu.py
@@ -0,0 +1,41 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from random import randint
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+# Locators
+SHOP_BY_BRANDS = (By.XPATH, "(//a[@class='site-nav--link'])[2]")
+SHOP_BY_BRANDS_BY_MENU = (By.XPATH, "//span[@class='icon icon-arrow-down']") # (By.XPATH, "//li[@class='site-nav--has-dropdown site-nav--has-dropdown-grandchild']")
+
+# Explicit wait
+wait = WebDriverWait(driver, 15)
+
+# 1. Open the url
+driver.get( 'https://www.autocareparts.com/' )
+
+# 2. Hover over Shop By Brand
+target = wait.until(EC.visibility_of_element_located(SHOP_BY_BRANDS))
+actions = ActionChains(driver)
+actions.move_to_element(target)
+sleep(2)
+actions.perform()
+
+# 3. Count how many elements has Shop By Brand 9
+expected_quantity = 9
+quantity_elements = len(driver.find_elements(*SHOP_BY_BRANDS_BY_MENU))
+if expected_quantity == quantity_elements:
+    print(f'Ok, we have: {quantity_elements} elements')
+else:
+    print(f'Not ok, we have: {quantity_elements} elements')
+
+# Sleep to see what we have
+sleep(2)
+
+# Driver quit
+driver.quit()
